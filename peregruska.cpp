867ac2b581adc429b17ce8372d9b3107263cd1b1
ABERILL <117598564+ABERILL@users.noreply.github.com>
2022-12-17 22:25:28 +0300

    peregruska

--- peregruska.cpp
+++ peregruska.cpp
@@ -1,137 +1,288 @@
-#16
-#include <string>
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-class Goods
+class Account
 {
-private:
-
-    string name;   //название
-    string date;    //дата оформления
-    int price;       //цена
-    int amount;   //количество
-    int number;   //номер накладной
-
 public:
-
-    //конструктор по умолчанию
-    Goods()
-    {
-        name = "";
-        date = "";
-        price = 0;
-        amount = 0;
-        number = 0;
-
+    string name;
+    int number;
+    int percent;
+    int summa;
+    Account() {}
+    Account(string Name, int Number, int Summa, int Percent) { name = Name; number = Number; summa = Summa; percent = Percent; };
+    void Read();
+    void Display();
+    void Change_Owner();
+    void Withdraw();
+    void Deposit();
+    void Percent();
+    void Exchange();
+    void In_Words();
+    Account operator + (const Account& other) {
+        Account temp;
+        temp.name = this->name + other.name;
+        temp.number = this->number + other.number;
+        temp.percent = this->percent + other.percent;
+        temp.summa = this->summa + other.summa;
+        return temp;
     }
-
-    //конструктор с параметрами
-    Goods(string s1, string s2, int p, int a, int n)
-    {
-        name = s1;
-        date = s2;
-        price = p;
-        amount = a;
-        number = n;
-    }
-
-    //получить название товара
-    string GetName()
-    {
-        return name;
-    }
-
-    //задать название товара
-    void SetName(string s)
-    {
-        name = s;
-    }
-
-    //получить дату оформления
-    string GetDate()
-    {
-        return date;
-    }
-
-    //задать дату оформления
-    void SetDate(string s)
-    {
-        date = s;
-    }
-
-    //получить цену товара
-    int GetPrice()
-    {
-        return price;
-    }
-
-    //задать цену товара
-    void SetPrice(int p)
-    {
-        price = p;
-    }
-
-    //получить количество товара
-    int GetAmount()
-    {
-        return amount;
-    }
-
-    //задать количество товара
-    void SetAmount(int a)
-    {
-        amount = a;
-    }
-
-    //получить номер накладной
-    int GetNumber()
-    {
-        return number;
-    }
-
-    //задать номер накладной
-    void SetNumber(int n)
-    {
-        number = n;
-    }
-
-    //получить цену товара в виде строки
-    string ToStringPrice()
-    {
-        string s = "";
-        int i;
-
-        for (i = price; i > 9; i /= 10) s += to_string(i % 10);
-
-        s += to_string(i);
-        reverse(s.begin(), s.end());
-        return s;
-    }
-
-    //получить общую стоимость
-    int GetTotalCost()
-    {
-        return price * amount;
-    }
-
-    //получить стоимость заданного числа товара
-    int GetCost(int n)
-    {
-        return price * n;
-    }
-
-    //количество товара увеличить на заданное число
-    void AmountIncrease(int n)
-    {
-        amount += n;
-    }
-
-    //количество товара уменьшить на заданное число
-    void AmountDecrease(int n)
-    {
-        amount -= n;
-    }
-
 };
+
+void Account::Read()
+{
+    cout << "Enter the Last Name: " << endl;
+    cin >> name;
+    cout << "Enter the bank account number: " << endl;
+    cin >> number;
+    cout << "Enter the amount: " << endl;
+    cin >> summa;
+    cout << "Enter the percent: " << endl;
+    cin >> percent;
+}
+
+void Account::Display()
+{
+    cout << "Last Name: " << name << endl;
+    cout << "Bank account number: " << number << endl;
+    cout << "Balance: " << summa << endl;
+    cout << "Percent: " << percent << endl;
+}
+
+void Account::Change_Owner()
+{
+    cout << "Enter the last name of the new owner: " << endl;
+    cin >> name;
+}
+
+void Account::Withdraw()
+{
+    int s;
+    cout << "Enter the amount you would like to withdraw " << endl;
+    cin >> s;
+
+    if (s > summa)
+        cout << "Insufficient funds " << endl;
+    else
+    {
+        summa -= s;
+        cout << "Remaining amount: " << summa << endl;
+    }
+}
+
+void Account::Deposit()
+{
+    int s;
+    cout << "Enter the amount you would like to deposit: " << endl;
+    cin >> s;
+
+    summa += s;
+    cout << "Balance: " << summa << endl;
+}
+
+void Account::Percent()
+{
+    cout << "Interest accured " << endl;
+    cout << "The interest on your deposit is equal to: " << percent << endl;
+    summa = summa + ((summa * percent) / 100);
+    cout << "The amount after interest is equal to: " << endl;
+    cout << summa << endl;
+}
+
+
+
+void Account::Exchange()
+{
+    int number;
+    cout << "Which currence you would like to exchange to: " << endl;
+    cout << "1) USD " << endl;
+    cout << "2) EUR " << endl;
+    cin >> number;
+    if (number == 1)
+        cout << summa << "RUB = " << summa / 62 << "USD" << endl;
+    else if (number == 2)
+        cout << summa << "RUB = " << summa / 66 << "EUR" << endl;
+    else
+        cout << "Incorrect Input " << endl;
+
+}
+
+char* numeral(int n, size_t size, char* buf)
+{
+    char* one[] = { "","one ","two ","three ","four ","five ","six ","seven ","eight ","nine ",
+                  "ten ","eleven ","twelve ","thirteen ","fourteen ","fifteen ",
+                  "sixteen ","seventeen ","eighteen ","nineteen " };
+    char* ten[] = { "","", "twenty ", "thirty ", "fourty ", "fifty ", "sixty ", "seventy ",
+                  "eighty ", "ninety " };
+
+    buf = new char[size];
+    sprintf(buf, "%u", n);
+    int len = strlen(buf);
+    int temp, temp2;
+    switch (len)
+    {
+    case 1:
+        cout << one[n];
+        break;
+    case 2:
+        if (n < 20)
+        {
+            cout << one[n];
+        }
+        else if (n < 100)
+        {
+            if ((n % 10) == 0) cout << ten[n / 10];
+            else cout << ten[n / 10] << one[n % 10];
+        }
+        break;
+    case 3:
+        temp = n / 100;
+        n -= (n / 100) * 100;
+        if (n < 20)
+        {
+            cout << one[temp] << "hundred " << one[n];
+
+        }
+        else if (n < 100)
+        {
+            if ((n % 10) == 0) cout << one[temp] << "hundred " << ten[n / 10];
+            else cout << one[temp] << "hundred " << ten[n / 10] << one[n % 10];
+        }
+        break;
+    case 4:
+        temp = n / 1000;
+        n -= (n / 1000) * 1000;
+        cout << one[temp] << "thousand ";
+        temp = n / 100;
+        n -= (n / 100) * 100;
+        if (n < 20)
+        {
+            cout << one[temp] << "hundred ";
+            cout << one[n];
+        }
+        else if (n < 100)
+        {
+            if ((n % 10) == 0) {
+                cout << one[temp] << "hundred ";
+                cout << ten[n / 10];
+            }
+            else {
+                cout << one[temp] << "hundred ";
+                cout << ten[n / 10];
+                cout << one[n % 10];
+            }
+        }
+        break;
+    case 5:
+        temp = n / 1000;
+        n -= (n / 1000) * 1000;
+        if (temp < 20) {
+            cout << one[temp] << "thousand ";
+        }
+        else if (temp < 100)
+        {
+            if ((temp % 10) == 0) cout << ten[temp / 10] << "thousand ";
+            else cout << ten[temp / 10] << one[temp % 10] << "thousand ";
+        }
+        temp = n / 100;
+        n -= (n / 100) * 100;
+        if (n < 20)
+        {
+            cout << one[temp] << "hundred ";
+            cout << one[n];
+        }
+        else if (n < 100)
+        {
+            if ((n % 10) == 0) {
+                cout << one[temp] << "hundred ";
+                cout << ten[n / 10];
+            }
+            else {
+                cout << one[temp] << "hundred ";
+                cout << ten[n / 10];
+                cout << one[n % 10];
+            }
+        }
+        break;
+    case 6:
+        temp2 = n / 1000;
+        n -= (n / 1000) * 1000;
+
+        temp = temp2 / 100;
+        temp2 -= temp * 100;
+
+        if (temp2 < 20) {
+            cout << one[temp] << "hundred  and " << one[temp2] << "thousand ";
+        }
+        else if (temp2 < 100)
+        {
+            if ((temp % 10) == 0) cout << one[temp] << "hundred  and " << ten[temp2 / 10] << "thousand ";
+            else cout << one[temp] << "hundred and " << ten[temp2 / 10] << one[temp2 % 10] << "thousand ";
+        }
+        temp = n / 100;
+        n -= (n / 100) * 100;
+        if (n < 20)
+        {
+            cout << one[temp] << "hundred ";
+            cout << one[n];
+
+        }
+        else if (n < 100)
+        {
+            if ((n % 10) == 0) {
+                cout << one[temp] << "hundred ";
+                cout << ten[n / 10];
+            }
+            else {
+                cout << one[temp] << "hundred ";
+                cout << ten[n / 10];
+                cout << one[n % 10];
+            }
+        }
+        break;
+    }
+    return buf;
+}
+
+
+void Account::In_Words()
+{
+    char* str;
+    char buf[100];
+    int choice = summa;
+    str = numeral(choice, 256, buf);
+    cout << str << endl;
+}
+
+
+
+
+int main()
+{
+    Account acc;
+
+    cout << "Personal Information: " << endl;
+    acc.Read();
+    cout << "Print data: " << endl;
+    acc.Display();
+    cout << "Change the account owner " << endl;
+    acc.Change_Owner();
+    cout << "Withdraw" << endl;
+    acc.Withdraw();
+    cout << "deposit" << endl;
+    acc.Deposit();
+    cout << "Exchange Currency" << endl;
+    acc.Exchange();
+    cout << "Print in words " << endl;
+    acc.In_Words();
+
+    Account acc2 = acc; //конструктор копирования
+    Account* acc3 = new Account("Vasya", 12, 13, 14); //конструктор инициализации
+    Account* acc4 = new Account(); //конструктор по умолчанию
+    Account acc5 = acc + acc2; //перегрузка
+    cout << endl << endl << "Overloaded Object" << endl;
+    acc5.Display();
+    Account account_arr[3]; //массив объектов
+    account_arr[0] = acc;
+    account_arr[1] = acc2;
+}
