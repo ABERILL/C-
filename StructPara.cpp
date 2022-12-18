7153666a65c1e587bf3283d2bf946a235c3fa5a8
ABERILL <117598564+ABERILL@users.noreply.github.com>
2022-12-18 22:21:05 +0300

    Структура-пара

--- StructPara.cpp
+++ StructPara.cpp
@@ -1,27 +1,74 @@
-#include "StructPara.h"
 #include <iostream>
-
-void init(Para p)
-{
-	p.first = 0;
-	p.second = 0;
+using namespace std;
+class TMun {
+public:
+    TMun(float x, float y) :first(x), second(y) {}
+    ~TMun() {};
+    TMun* Read();
+    void Display();
+    float Distance();
+private:
+    float first;
+    float second;
+};
+TMun* TMun::Read() {
+    float x, y;
+    cout << "Input x: ";
+    cin >> x;
+    if (!cin) {
+        cout << "\nInput error!" << endl;
+        cin.clear();
+        return NULL;
+    }
+    cout << "Input y: ";
+    cin >> y;
+    if (!cin) {
+        cout << "\nInput error!" << endl;
+        cin.clear();
+        return NULL;
+    }
+    first = x;
+    second = y;
+    return this;
+}
+void TMun::Display() {
+    cout << "\tfirst: " << first << "\tsecond: " << second << endl;
+}
+float TMun::Distance() {
+    return sqrt(first * first + second * second);
+}
+TMun* Make_Mun() {
+    float x, y;
+    cout << "Input x: ";
+    cin >> x;
+    if (!cin) {
+        cout << "\nInput error!" << endl;
+        cin.clear();
+        return NULL;
+    }
+    cout << "Input y: ";
+    cin >> y;
+    if (!cin) {
+        cout << "\nInput error!" << endl;
+        cin.clear();
+        return NULL;
+    }
+    TMun* mun = new TMun(x, y);
+    return mun;
 }
 
-void read(Para p)
+int main()
 {
-	std::cout << "first = ";
-	std::cin >> p.first;
-	std::cout << "second = ";
-	std::cin >> p.second;
-}
-
-void display(Para p)
-{
-	std::cout << "first = " << p.first << '\n';
-	std::cout << "second = " << p.second;
-}
-
-int nod(Para p)
-{
-	return 0;
+    TMun* m;
+    if (m = Make_Mun()) { // Создаем
+        m->Display();
+    }
+    if (m->Read()) { // Читаем новые координаты
+        m->Display();
+    }
+    if (m) { // Выводим дистанцию
+        cout << "\nDistance: " << m->Distance() << endl;
+    }
+    system("Pause");
+    return 0;
 }
